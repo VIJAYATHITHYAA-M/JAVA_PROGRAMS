1b16afbed805f0a60d71b24a906e4bfd9059519a
VIJAY ATHITHYAA M <vijayathithyaa2206@gmail.com>
2025-07-29 19:32:43 +0530

    Add files via upload
    SINGLE ARRAY

---
+++ arr.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.lang.*;
+public class arr
+{
+public static void main(String args[])
+{
+int[] a=new int[3];
+a[0]=10;
+a[1]=20;
+a[2]=30;
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
